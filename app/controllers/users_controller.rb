c9d7148ec73f80ef8d5c367411920f8f7830872b
Michael Medis <TheMedisGroup@gmail.com>
2015-11-13 10:43:58 -0500

    Mailgun Implemented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,21 @@
 class UsersController < ApplicationController
 
-  def index
-  end
+  # POST /users
+  # POST /users.json
+  def create
+    @user = User.new(params[:user])
 
-  def show
-  end
+    respond_to do |format|
+      if @user.save
+        # Tell the UserMailer to send a welcome email after save
+        UserMailer.welcome_email(@user).deliver_later
 
-  def edit
+        format.html { redirect_to(@user, notice: 'User was successfully created.') }
+        format.json { render json: @user, status: :created, location: @user }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
-
-  def update
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:update, :show, :index]
-  get 'welcome/index'
 
+  get 'welcome/index'
   get 'welcome/about'
+
+  post :incoming, to: 'incoming#create'
 
   root to: 'welcome#index'
 
