10e4b8a6f64800e8e7fc9d65a87076d72d2a487a
Michael Medis <TheMedisGroup@gmail.com>
2015-11-24 11:16:43 -0500

    adding user to topic in incoming controller, plus some

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -7,20 +7,20 @@ class IncomingController < ApplicationController
     puts "testing 2 "
     users = User.where(email: params[:sender])
     puts "hello there 3"
-    if users.count == 0 || @topic.count == 0
+    if users.count == 0
       puts "hello there 4"
       User.invite!(email: params[:sender], name: params[:sender])
       puts "hello there 5"
     else
       @user = users.first
       puts params[:subject]
-      @topic = Topic.find_or_create_by(title: params[:subject])
+      @topic = Topic.find_or_create_by(title: params[:subject], user: @user)
       puts @topic
       @url = params["stripped-text"]
       puts @url
       @bookmark = Bookmark.new(user: @user, topic: @topic, url: @url)
       puts @bookmark
-      authorize @bookmark
+      # authorize @bookmark
       @bookmark.save
     end
     head 200
