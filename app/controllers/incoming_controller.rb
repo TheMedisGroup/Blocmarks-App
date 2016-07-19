8370e9344a44d06781defa433bdbcb58f4538847
Michael Medis <TheMedisGroup@gmail.com>
2016-07-19 14:25:05 -0400

    cleaned up incoming_controller.rb

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -2,15 +2,10 @@ class IncomingController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
 
   def create
-    puts "hello there"
     puts params[:sender]
-    puts "testing 2 "
     users = User.where(email: params[:sender])
-    puts "hello there 3"
     if users.count == 0
-      puts "hello there 4"
       User.invite!(email: params[:sender], name: params[:sender])
-      puts "hello there 5"
     else
       @user = users.first
       puts params[:subject]
